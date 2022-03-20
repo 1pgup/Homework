ec6ca6a69fe4de9565f075948eefc05ca0314391
Emill Sharafutdinov <emill.sh@yandex.ru>
2022-03-20 16:32:34 +0300

    * commit

---
+++ hw-1/project/src/utils.c
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+size_t timer_from(int from) {
+    size_t counter = 0;
+    if (from < 1) {
+        return counter;
+    }
+    for (int i = from; ; --i) {
+        ++counter;
+        if (i == 0) {
+            printf("%d\n", i);
+            break;
+        }
+    printf("%d ", i);
+    }
+    return counter;
+}
+
+int custom_pow(int base, int power) {
+    if (((base == 0) && (power == 0)) || (power < 0)) {
+        return ERR_RETURN;
+    }
+    int result = 1;
+    for (int i = 1; i <= power; i++) {
+        result *= base;
+    }
+    return result;
+}
+
