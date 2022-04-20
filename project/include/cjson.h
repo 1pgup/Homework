1c92939e33c422d66fae8740faed111f5345bbd4
Emil Sharafutdinov <100351034+1pgup@users.noreply.github.com>
2022-04-20 18:00:18 +0300

    Update cjson

--- project/include/cjson.h
+++ project/include/cjson.h
@@ -1,19 +1,16 @@
 #ifndef PROJECT_INCLUDE_CJSON_H_
 #define PROJECT_INCLUDE_CJSON_H_
 
-#include <stdbool.h>
+int get_date(const char* path_to_eml, char* date);
+int get_from(const char* path_to_eml, char* from);
+int get_parts(const char* path_to_eml);
+int get_to(const char* path_to_eml, char* to);
 
-#define ERROR_NULL_PTR (-1)
-
-int is_content_type_found(char* filename);
-int is_multipart(char* filename, int content_type_is_found);
-
-int get_boundary(char* filename, char *boundary, int multipart_is_found);
-int get_parts(int is_content_type, int is_multipart, int count_of_boundaries);
-
-int get_date(char* filename, char *date);
-int get_from(char* filename, char* from);
-int get_to(char* filename, char* to);
-
+typedef enum {
+    MAX_SIZE_OF_FROM = 1000,
+    MAX_SIZE_OF_TO = 1000,
+    MAX_SIZE_OF_DATE = 1000,
+    MAX_SIZE_OF_SECONDARY_WORD = 100
+} sizes_t;
 
 #endif  // PROJECT_INCLUDE_CJSON_H_
