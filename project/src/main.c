bc558873ab4908a0e27e9ad3a30c1d14862de903
Emil Sharafutdinov <100351034+1pgup@users.noreply.github.com>
2022-04-20 17:52:43 +0300

    Update main

--- project/src/main.c
+++ project/src/main.c
@@ -1,70 +1,47 @@
+#include <ctype.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "cjson.h"
 
-#define ERROR_NULL_PTR (-1)
-#define ERROR_SMTH_WRONG  (-2)
+#define ERROR_NULL_PTR             (-1)
+#define ERROR_WRONG_VALUE_OF_ARGS  (-2)
 
-// check
-
-/*printf("%s\n", from);
-printf("%s\n", to);
-printf("%s\n", date);
-printf("%d\n\n", content_type_is_found);
-printf("%d\n\n", multipart_is_found);
-printf("%s\n\n", boundary);
-printf("%d\n\n", count_of_boundaries);*/
-
-int main(int argc, char *argv[]) {
+int main(int argc, const char **argv) {
     if (argc != 2) {
-        return ERROR_SMTH_WRONG;
+        return ERROR_WRONG_VALUE_OF_ARGS;
     }
 
-    char *s = argv[1];
-    if (!s) {
+    const char *path_to_eml = argv[1];
+    if (!path_to_eml) {
         return ERROR_NULL_PTR;
     }
 
-    char from[1000] = {0};
-    char to[1000] = {0};
-    char date[1000] = {0};
-    char boundary[1000] = {0};
-
     // From
-    if (get_from(s, from) == ERROR_NULL_PTR) {
-        return ERROR_SMTH_WRONG;
+    char from[MAX_SIZE_OF_FROM] = {0};
+    if (get_from(path_to_eml, from)) {
+        printf("Something went wrong");
     }
 
     // To
-    if (get_to(s, to) == ERROR_NULL_PTR) {
-        return ERROR_SMTH_WRONG;
+    char to[MAX_SIZE_OF_TO] = {0};
+    if (get_to(path_to_eml, to)) {
+        printf("Something went wrong");
+    }
+    // Date
+    char date[MAX_SIZE_OF_DATE] = {0};
+    if (get_date(path_to_eml, date)) {
+        printf("Something went wrong");
     }
 
-    //  Date
-    if (get_date(s, date) == ERROR_NULL_PTR) {
-        return ERROR_SMTH_WRONG;
+    // parts
+    int parts = get_parts(path_to_eml);
+    if (parts < 0) {
+        printf("Something went wrong");
     }
 
-    //  Conten-Type
-    int content_type_is_found = is_content_type_found(s);
-    if (content_type_is_found == ERROR_NULL_PTR) {
-        return ERROR_SMTH_WRONG;
-    }
-
-    //   multipart
-    int multipart_is_found = is_multipart(s, content_type_is_found);
-    if (multipart_is_found == ERROR_NULL_PTR) {
-        return ERROR_SMTH_WRONG;
-    }
-
-    //  boundary
-    int count_of_boundaries = get_boundary(s, boundary, multipart_is_found);
-
-    //  parts
-    int parts = get_parts(content_type_is_found, multipart_is_found, count_of_boundaries);
-
-    printf("%s|%s|%s|%d", from, to, date, parts);
+    printf("%s|%s|%s|%d\n", from, to, date, parts);
 
     return 0;
 }
